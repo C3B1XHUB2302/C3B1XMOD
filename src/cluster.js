7f7219ec95353f50e58c34bed51ec53eedb567ab
AeroX <aeroxdevs@gmail.com>
2026-04-04 13:20:13 +0530

    Update cluster.js

--- src/cluster.js
+++ src/cluster.js
@@ -4,7 +4,7 @@ const syncEmojis = require("./utils/emojiSync");
 
 const token = process.env.DISCORD_TOKEN || require("./config.json").token;
 
-const manager = new ClusterManager(path.join(__dirname, "app.js"), {
+const manager = new ClusterManager(path.join(__dirname, "index.js"), {
   totalShards:      "auto",
   shardsPerClusters: 2,
   mode:             "process",
